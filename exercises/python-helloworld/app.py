c8163ca5336d297b3778994c5d28c55739eefbcb
akshayb7 <akshay10.bhardwaj@gmail.com>
2021-06-15 21:52:53 +0530

    ADD: Logging functionality to the helloworld app

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask import json
+import time
+import logging
+
+logging.basicConfig(filename='app.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
 @app.route("/status")
 def status():
+
     response = app.response_class(
         response=json.dumps({"result": "OK - healthy"}),
         status=200,
         mimetype='application/json'
     )
+    
+    app.logger.info('Status request successfull')
     return response
 
 @app.route("/metrics")
@@ -23,6 +31,8 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
+
+    app.logger.info('Metrics request successfull')
     return {"data": {"UserCount": 140, "UserCountActive": 23}}
 
 if __name__ == "__main__":
